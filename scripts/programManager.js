b1a3cc571d18ab0eb7ea4bd0faf5b10e78262e6f
Halie Chmura <haliechm@live.unc.edu>
2020-01-27 18:34:38 -0500

    Figuring out JS (IP)

---
+++ scripts/programManager.js
@@ -0,0 +1,32 @@
+
+
+alert("Today's date is May");
+
+//var difficulty;
+//function ProgramManager(difficulty) {
+//    this.difficulty = difficulty;
+//}
+//
+//
+//let ProgramManager = {
+//    startGame: function() {
+//        switch(difficulty) {
+//                
+//                
+//            case "beginner": //go to beginner page
+//            break;
+//                
+//            case "intermediate": // go to intermediate page
+//            break;
+//                
+//            case "advanced": // go to advanced page
+//            break;
+//                
+//            case "expert": //go to expert page
+//            break;
+//        }
+//        
+//    }
+//    
+//    
+//}
